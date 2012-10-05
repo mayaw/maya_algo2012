23f090e7abdf75ac96469cf82df2504c7710bd5d
Maya Weinstein <luckypapaya@gmail.com>
2012-10-05 18:48:36 -0400

    homework

---
+++ fireworks/src/particle.cpp
@@ -0,0 +1,9 @@
+//
+//  particle.cpp
+//  fireworks
+//
+//  Created by Maya Weinstein on 10/5/12.
+//
+//
+
+#include "particle.h"
---
+++ fireworks/src/particle.h
@@ -0,0 +1,14 @@
+//
+//  particle.h
+//  fireworks
+//
+//  Created by Maya Weinstein on 10/5/12.
+//
+//
+
+#ifndef fireworks_particle_h
+#define fireworks_particle_h
+
+
+
+#endif
